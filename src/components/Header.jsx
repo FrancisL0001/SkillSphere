421527cdbda55ad0241e5a1e046bf66c9c8831b1
Francois LEUTOU <francoisleuts@gmail.com>
2025-07-06 20:01:08 +0100

    Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({page}) => {
     <div className="w-full items-center mb-4 py-2 border-b-2 border-0">
         <div className="flex flex-row justify-center md:justify-between">
             <Link to={'/'}>
-                <img src="\src\assets\skillsphere.png" alt="SkillSphere Logo" className="max-h-16 mx-auto pl-4"/>
+                <img src="\skillsphere.png" alt="SkillSphere Logo" className="max-h-16 mx-auto pl-4"/>
             </Link>
             <div className="hidden md:flex md:mr-6">
                 <Navbar page = {page}/>
