a3dde609e6461a1430e6bbf12bdc824b59c86f04
Francois LEUTOU <francoisleuts@gmail.com>
2025-07-06 19:47:16 +0100

    Slight modification on the footer component for deployment.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./navbar.jsx"
+import Navbar from "./Navbar.jsx";
+
 
 const Footer = ({page}) => {
   return (
     <div className="min-w-full justify-center mx-auto">
         <div className="fixed bottom-0 bg-white w-full md:hidden ">
-            <Navbar page={page}/>
+          <Navbar page={page} />
         </div>
         <div className="hidden md:flex flex-col">
             <div className="flex flex-row justify-center gap-4">
@@ -15,7 +16,7 @@ const Footer = ({page}) => {
             <p className="text-xs text-black text-center">&copy;2025 Because I decided </p>
         </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
